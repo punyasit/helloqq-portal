2c1d86d230a479f1859436b1fe9549aa6ce3810d
Punyasit J <punyasit.innovation@hotmail.com>
2018-05-12 01:01:34 +0700

    + add new product management pages. + add new admin management pages.

---
+++ HelloQQPortal/Controllers/adminController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloQQPortal.Controllers
+{
+    public class adminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Administrator.";
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        // GET: Admin
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+
+        // GET: Admin
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+
+        public ActionResult UserList()
+        {
+            return View();
+        }
+
+        // GET: Admin
+        public ActionResult UserDetail()
+        {
+            return View();
+        }
+
+        public ActionResult ManualList()
+        {
+            return View();
+        }
+
+        // GET: Admin
+        public ActionResult ManualDetail()
+        {
+            return View();
+        }
+
+        public ActionResult FaqList()
+        {
+            return View();
+        }
+
+        // GET: Admin
+        public ActionResult FaqDetail()
+        {
+            return View();
+        }
+    }
+}
